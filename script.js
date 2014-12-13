e6b0e51d4c3c8c7fc23fa6a238ad2911dfd0ce97
Zack Hanni <zack.hanni@gmail.com>
2014-12-13 14:46:15 -0500

    initial commit of script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+var bill = prompt("How much did your bill cost?");
+var percent = prompt("what percent tip do you want to pay?");
+alert("You should pay $" + (percent * 0.01 * bill) + " in tip for your meal");
