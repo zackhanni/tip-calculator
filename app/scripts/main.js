4b2da830a372f8c3d3232ef4477c52bbbd4d8fdc
Ariel Kirkwood <ariel.kirkwood@me.com>
2014-12-14 22:00:54 -0500

    JSHint fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,3 @@
-var bill = prompt("How much did your bill cost?");
-var percent = prompt("what percent tip do you want to pay?");
-alert("You should pay $" + (percent * 0.01 * bill) + " in tip for your meal");
+var bill = window.prompt('How much did your bill cost?');
+var percent = window.prompt('what percent tip do you want to pay?');
+window.alert('You should pay $' + (percent * 0.01 * bill) + ' in tip for your meal');
