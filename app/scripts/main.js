92b2ce6b2d2f88f23af15278fc532c69dd8d82bc
Ariel Kirkwood <ariel.kirkwood@me.com>
2014-12-14 21:15:38 -0500

    Moved script.js to main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,3 @@
-console.log('\'Allo \'Allo!');
+var bill = prompt("How much did your bill cost?");
+var percent = prompt("what percent tip do you want to pay?");
+alert("You should pay $" + (percent * 0.01 * bill) + " in tip for your meal");
--- script.js
+++
@@ -1,3 +0,0 @@
-var bill = prompt("How much did your bill cost?");
-var percent = prompt("what percent tip do you want to pay?");
-alert("You should pay $" + (percent * 0.01 * bill) + " in tip for your meal");
